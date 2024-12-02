786ed8b6f00970e058221e57bd5c37e18cbe1949
Julia <yusmanankova_1@edu.hse.ru>
2024-12-02 22:22:00 +0300

    Create database

---
+++ schedule_app/backend/core/config.py
@@ -0,0 +1,9 @@
+from pydantic import BaseSettings
+
+class Settings(BaseSettings):
+    DATABASE_URL: str = "postgresql://user:password@localhost:5432/schedule_db"
+
+    class Config:
+        env_file = ".env"
+
+settings = Settings()
